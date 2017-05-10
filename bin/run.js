781992c7383212a4ba79f04a2fc2d99d30aeed3b
Khan, Daniel <Daniel.Khan@dynatrace.com>
2017-05-10 16:07:09 +0200

    Service Subscription

--- bin/run.js
+++ bin/run.js
@@ -1,12 +1,26 @@
 'use strict';
 
 const http = require('http');
+const request = require('superagent');
 const config = require('../config');
 const log = config.log();
 const service = require('../server/service')(config);
 const server = http.createServer(service);
-server.listen(process.env.port || 3001);
+server.listen();
 
 server.on('listening', function() {
     log.info(`Time Service is listening on ${server.address().port} in ${service.get('env')} mode`);
+
+    const mainUrl = 'http://127.0.0.1:3000';
+
+    const announce = () => {
+        request.put(`${mainUrl}/service/time/${server.address().port}`)
+        .end((err) => {
+            if(err) log.error(err);
+        })
+    }
+
+    announce();
+    setInterval(announce, 20 * 1000);
+
 });
